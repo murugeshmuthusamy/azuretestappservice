fb9464f9bf9bc8daf3f2445a4a2d887ad1cbb130
Murugesan Muthusamy <murugesan.muthusamy@intergy.com.au>
2021-05-18 09:49:56 +0530

    Test file

--- test.php
+++ test.php
@@ -7,3 +7,8 @@ echo 'BASEURL---'.$_ENV['BASEURL'];
 //echo 'APPSETTING_BASEURL---'.$_ENV['APPSETTING_BASEURL'];
 echo '<br>';
 echo 'DBNAME---'.$_ENV['MYSQLCONNSTR_DBNAME'];
+echo '<br>';
+echo 'ENV Var';
+echo 'ENVBASEURL--'.env('BASEURL');
+echo '<br>';
+echo 'ENVDBNAME--'.env('MYSQLCONNSTR_DBNAME');
